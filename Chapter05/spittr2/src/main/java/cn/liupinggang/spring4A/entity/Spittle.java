5202fba767c000468a00c1a7735078b7c62fd5a7
1135142128@qq.com <1135142128@qq.com>
2022-08-07 11:48:21 +0800

    第二次提交

--- Chapter05/spittr2/src/main/java/cn/liupinggang/spring4A/entity/Spittle.java
+++ Chapter05/spittr2/src/main/java/cn/liupinggang/spring4A/entity/Spittle.java
@@ -1,29 +1,28 @@
 package cn.liupinggang.spring4A.entity;
 
+import java.util.Date;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import java.util.Date;
-import java.util.Objects;
-
 public class Spittle {
-    private final long id;
+
+    private final Long id;
     private final String message;
     private final Date time;
     private Double latitude;
     private Double longitude;
 
-    public Spittle(){
+    public Spittle(String message, Date time) {
+        this(null, message, time, null, null);
+    }
 
-    }
-    public Spittle(String message,Date time){
-        this(message,time,null,null);
-    }
-    public Spittle(String message, Date time, Double latitude, Double longitude) {
+    public Spittle(Long id, String message, Date time, Double longitude, Double latitude) {
+        this.id = id;
         this.message = message;
         this.time = time;
-        this.latitude = latitude;
         this.longitude = longitude;
+        this.latitude = latitude;
     }
 
     public long getId() {
@@ -38,24 +37,22 @@ public class Spittle {
         return time;
     }
 
+    public Double getLongitude() {
+        return longitude;
+    }
+
     public Double getLatitude() {
         return latitude;
     }
 
-    public Double getLongitude() {
-        return longitude;
-    }
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Spittle spittle = (Spittle) o;
-        return new EqualsBuilder().append(id, spittle.id).append(time, spittle.time).isEquals();
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that, "id", "time");
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(id).append(time).toHashCode();
+        return HashCodeBuilder.reflectionHashCode(this, "id", "time");
     }
 
 }
